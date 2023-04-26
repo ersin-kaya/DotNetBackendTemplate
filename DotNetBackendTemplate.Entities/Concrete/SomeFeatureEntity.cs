b5c99b2a729d71c2cff157009ba077017f468ba0
Ersin Kaya <79023498+ersin-kaya@users.noreply.github.com>
2023-04-26 07:12:04 +0300

    SomeFeatureEntity.cs created.

---
+++ DotNetBackendTemplate.Entities/Concrete/SomeFeatureEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using DotNetBackendTemplate.Core.Entities;
+
+namespace DotNetBackendTemplate.Entities.Concrete
+{
+	public class SomeFeatureEntity : IEntity
+	{
+		public SomeFeatureEntity()
+		{
+		}
+	}
+}
+
