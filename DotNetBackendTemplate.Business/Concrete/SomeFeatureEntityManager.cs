05adfeb956abfcb3c9b35879ddb8f174b0021f87
Ersin Kaya <79023498+ersin-kaya@users.noreply.github.com>
2023-04-26 18:15:37 +0300

    SomeFeatureEntityManager.cs created.

---
+++ DotNetBackendTemplate.Business/Concrete/SomeFeatureEntityManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using DotNetBackendTemplate.Business.Abstract;
+using DotNetBackendTemplate.Core.Utilities.Results.Abstract;
+using DotNetBackendTemplate.Core.Utilities.Results.Concrete;
+using DotNetBackendTemplate.DataAccess.Abstract;
+using DotNetBackendTemplate.Entities.Concrete;
+
+namespace DotNetBackendTemplate.Business.Concrete
+{
+	public class SomeFeatureEntityManager : ISomeFeatureEntityService
+	{
+        ISomeFeatureEntityDal _someFeatureEntityDal;
+
+		public SomeFeatureEntityManager(ISomeFeatureEntityDal someFeatureEntityDal)
+		{
+            _someFeatureEntityDal = someFeatureEntityDal;
+		}
+
+        public IResult Add(SomeFeatureEntity someFeatureEntity)
+        {
+            _someFeatureEntityDal.Add(someFeatureEntity);
+            return new SuccessResult();
+        }
+
+        public IResult Delete(SomeFeatureEntity someFeatureEntity)
+        {
+            _someFeatureEntityDal.Delete(someFeatureEntity);
+            return new SuccessResult();
+        }
+
+        public IDataResult<List<SomeFeatureEntity>> GetAll()
+        {
+            return new SuccessDataResult<List<SomeFeatureEntity>>();
+        }
+
+        public IDataResult<SomeFeatureEntity> GetById(int someFeatureEntityId)
+        {
+            return new SuccessDataResult<SomeFeatureEntity>();
+        }
+
+        public IResult Update(SomeFeatureEntity someFeatureEntity)
+        {
+            _someFeatureEntityDal.Update(someFeatureEntity);
+            return new SuccessResult();
+        }
+    }
+}
+
