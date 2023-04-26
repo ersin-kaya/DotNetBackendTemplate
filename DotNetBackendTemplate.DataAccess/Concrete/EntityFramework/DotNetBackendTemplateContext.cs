72ed61e447b949894da861ce45905a5734e1394e
Ersin Kaya <79023498+ersin-kaya@users.noreply.github.com>
2023-04-26 07:48:48 +0300

    DotNetBackendTemplateContext.cs created.

---
+++ DotNetBackendTemplate.DataAccess/Concrete/EntityFramework/DotNetBackendTemplateContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using DotNetBackendTemplate.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNetBackendTemplate.DataAccess.Concrete.EntityFramework
+{
+	public class DotNetBackendTemplateContext : DbContext
+	{
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost,1234;Database=DotNetBackendTemplate;");
+        }
+
+        public DbSet<SomeFeatureEntity> SomeFeatureEntities { get; set; }
+    }
+}
+
