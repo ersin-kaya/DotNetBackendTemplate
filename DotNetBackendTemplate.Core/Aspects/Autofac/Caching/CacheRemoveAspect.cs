b1b3c7cbf6f30e876596bc6fd9842ed27dea2182
Ersin Kaya <79023498+ersin-kaya@users.noreply.github.com>
2023-04-28 00:51:51 +0300

    Cache Aspects created.

---
+++ DotNetBackendTemplate.Core/Aspects/Autofac/Caching/CacheRemoveAspect.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Castle.DynamicProxy;
+using DotNetBackendTemplate.Core.CrossCuttingConcerns.Caching;
+using DotNetBackendTemplate.Core.Utilities.Interceptors;
+
+namespace DotNetBackendTemplate.Core.Aspects.Autofac.Caching
+{
+    public class CacheRemoveAspect : MethodInterception
+    {
+        private string[] _patterns;
+        private ICacheManager _cacheManager;
+
+        public CacheRemoveAspect(params string[] patterns)
+        {
+            _patterns = patterns;
+            _cacheManager = ServiceTool.ServiceProvider.GetService<ICacheManager>();
+        }
+
+        protected override void OnSuccess(IInvocation invocation)
+        {
+            _cacheManager.RemoveByPattern(_patterns);
+        }
+    }
+}
+
