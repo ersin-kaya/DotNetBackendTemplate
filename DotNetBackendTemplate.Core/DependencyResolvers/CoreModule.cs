824a666eb62d2d9d8cd54b7dc64bed975b6195c2
Ersin Kaya <79023498+ersin-kaya@users.noreply.github.com>
2023-04-28 01:12:28 +0300

    CoreModule.cs, ServiceCollectionExtensions.cs created.

---
+++ DotNetBackendTemplate.Core/DependencyResolvers/CoreModule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using DotNetBackendTemplate.Core.Utilities.IoC;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DotNetBackendTemplate.Core.DependencyResolvers
+{
+    public class CoreModule : ICoreModule
+    {
+        public void Load(IServiceCollection serviceCollection)
+        {
+            
+        }
+    }
+}
+
