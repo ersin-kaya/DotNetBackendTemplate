df9788c405412a40bdb4fd39ba7a31449e755972
Ersin Kaya <79023498+ersin-kaya@users.noreply.github.com>
2023-04-28 01:29:43 +0300

    In-Memory Caching

--- DotNetBackendTemplate.Core/DependencyResolvers/CoreModule.cs
+++ DotNetBackendTemplate.Core/DependencyResolvers/CoreModule.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using DotNetBackendTemplate.Core.CrossCuttingConcerns.Caching;
+using DotNetBackendTemplate.Core.CrossCuttingConcerns.Caching.Microsoft;
 using DotNetBackendTemplate.Core.Utilities.IoC;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -6,9 +8,10 @@ namespace DotNetBackendTemplate.Core.DependencyResolvers
 {
     public class CoreModule : ICoreModule
     {
-        public void Load(IServiceCollection serviceCollection)
+        public void Load(IServiceCollection services)
         {
-            
+            services.AddMemoryCache();
+            services.AddSingleton<ICacheManager, MemoryCacheManager>();
         }
     }
 }
