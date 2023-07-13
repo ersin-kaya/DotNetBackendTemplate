4c03ee5616f53b51e6cd2e74a9dead1ea36af83b
Ersin Kaya <79023498+ersin-kaya@users.noreply.github.com>
2023-07-13 13:24:33 +0300

    Automatically set created and last updated time for each record

--- DotNetBackendTemplate.DataAccess/Concrete/EntityFramework/BaseDbContext.cs
+++ DotNetBackendTemplate.DataAccess/Concrete/EntityFramework/BaseDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using DotNetBackendTemplate.Core.Entities;
 using DotNetBackendTemplate.Core.Entities.Concrete;
 using DotNetBackendTemplate.Entities.Concrete;
 using Microsoft.EntityFrameworkCore;
@@ -15,6 +16,26 @@ namespace DotNetBackendTemplate.DataAccess.Concrete.EntityFramework
         public DbSet<SomeFeatureEntity> SomeFeatureEntities { get; set; }
         public DbSet<OperationClaim> OperationClaims { get; set; }
         public DbSet<AccountOperationClaim> AccountOperationClaims { get; set; }
+
+        public override int SaveChanges()
+        {
+            var entries = ChangeTracker
+                .Entries()
+                .Where(e => e.Entity is IEntity
+                && (e.State == EntityState.Added
+                || e.State == EntityState.Modified));
+
+            foreach (var entityEntry in entries)
+            {
+                ((IEntity)entityEntry.Entity).LastUpdatedTime = DateTime.Now;
+
+                if (entityEntry.State == EntityState.Added)
+                {
+                    ((IEntity)entityEntry.Entity).CreatedTime = DateTime.Now;
+                }
+            }
+            return base.SaveChanges();
+        }
     }
 }
 
